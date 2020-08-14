a5ccfa50dd3af79df176efc688b72a076db2251e
Lukas Scheerer <lukasscheerer@spicysources.de>
2020-08-14 23:32:42 +0200

    Ensures sound thread safety.

--- app/src/main/java/de/spicysources/bubblepenetration/util/EffectTask.kt
+++ app/src/main/java/de/spicysources/bubblepenetration/util/EffectTask.kt
@@ -3,19 +3,17 @@ package de.spicysources.bubblepenetration.util
 import android.content.Context
 import android.media.MediaPlayer
 import de.spicysources.bubblepenetration.R
-import java.util.*
+import java.util.concurrent.ArrayBlockingQueue
 
 class EffectTask(private val context: Context, muted: Boolean) : Thread() {
 
     private var muted = false
     private var ingame = true
-    private val sounds: ArrayList<Int>
-    private val effectPlayer: HashMap<Int, MediaPlayer>
+    private val soundsToPlay = ArrayBlockingQueue<Int>(100)
+    private val effectPlayer = HashMap<Int, MediaPlayer>()
 
     init {
         this.muted = muted
-        sounds = ArrayList()
-        effectPlayer = HashMap()
         effectPlayer[R.raw.blubb] = MediaPlayer.create(context, R.raw.blubb)
         effectPlayer[R.raw.fart] = MediaPlayer.create(context, R.raw.fart)
         effectPlayer[R.raw.star] = MediaPlayer.create(context, R.raw.star)
@@ -23,17 +21,20 @@ class EffectTask(private val context: Context, muted: Boolean) : Thread() {
     }
 
     override fun run() {
+        soundsToPlay.clear()
         while (ingame) {
-            if (sounds.isNotEmpty() and !muted) {
-                if (effectPlayer[sounds[0]] == null) effectPlayer[sounds[0]] = MediaPlayer.create(context, sounds[0])
-                effectPlayer[sounds[0]]!!.start()
-                sounds.removeAt(0)
+            val soundIndex = soundsToPlay.take()
+            if (!muted) {
+                if (effectPlayer[soundIndex] == null) {
+                    effectPlayer[soundIndex] = MediaPlayer.create(context, soundIndex)
+                }
+                effectPlayer[soundIndex]!!.start()
             }
         }
     }
 
     fun playSound(index: Int) {
-        sounds.add(index)
+        soundsToPlay.put(index)
     }
 
     fun setIngame(ingame: Boolean) {
