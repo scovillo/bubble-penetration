a2f6959e3f30323655ec35180b20b0dd895dc4f7
David Link <davidlink@spicysources.de>
2023-03-29 23:42:59 +0200

    #3: Android und Backend Repos getrennt
    Changelog: changed

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM node:12-alpine
-COPY backend /bubble-backend
-WORKDIR /bubble-backend
-RUN npm install
-CMD ["node", "app.js"]
--- backend/db/db-setup.sql
+++
@@ -1,7 +0,0 @@
-create table if not exists highscores(
-    username  varchar(10) not null
-        constraint highscores_pkey
-            primary key,
-    highscore INT,
-    timestamp TIMESTAMP
-);
