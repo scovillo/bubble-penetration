261dba4cf1f8304a8059425b1eca8c909a2141de
Lukas Scheerer <lukasscheerer89@gmail.com>
2026-09-02 09:06:44 +0200

    Make profile list scrollable

--- app/src/main/java/org/codeberg/scovillo/bubble/ui/layout/UsernameSelectionLayout.kt
+++ app/src/main/java/org/codeberg/scovillo/bubble/ui/layout/UsernameSelectionLayout.kt
@@ -1,11 +1,13 @@
 package org.codeberg.scovillo.bubble.ui.layout
 
 import android.view.View
+import android.view.LayoutInflater
 import android.widget.AdapterView.OnItemClickListener
 import android.widget.ListView
 import org.codeberg.scovillo.bubble.MainActivity
 import org.codeberg.scovillo.bubble.R
 import org.codeberg.scovillo.bubble.api.UserResource
+import org.codeberg.scovillo.bubble.ui.BubbleFont
 
 class UsernameSelectionLayout(private val mainActivity: MainActivity) {
 
@@ -13,9 +15,13 @@ class UsernameSelectionLayout(private val mainActivity: MainActivity) {
         mainActivity.setContentView(R.layout.username_selection)
         val usernameAdapter = UsernameListAdapter(mainActivity, users)
         val usernameListView = (mainActivity.findViewById<View>(R.id.username_list) as ListView)
+        val addUserFooter = LayoutInflater.from(mainActivity)
+            .inflate(R.layout.username_list_footer, usernameListView, false)
+        BubbleFont.applyTo(addUserFooter)
+        usernameListView.addFooterView(addUserFooter, null, true)
         usernameListView.adapter = usernameAdapter
         usernameListView.onItemClickListener = OnItemClickListener { _, _, position, _ ->
-            mainActivity.selectUser(users[position])
+            if (position < users.size) mainActivity.selectUser(users[position])
         }
     }
 
